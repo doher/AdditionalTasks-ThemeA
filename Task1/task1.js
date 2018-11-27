921eb9b06d69eea17e200c12f22f20a74ae56a7d
doher <alexandr.pletezhov@gmail.com>
2018-11-27 15:22:15 +0300

    Added month name task

--- Task1/task1.js
+++ Task1/task1.js
@@ -1,10 +1,33 @@
-var message = prompt('Enter month name:', 'Juanary').toLowerCase();
+var monthName = prompt('Enter month name:').toLowerCase;
+console.log(monthName);
 
-switch (key) {
-    case value:
-        
+
+switch (monthName) {
+    case 'december':    
+    case 'january':
+    case 'february':
+        console.log('It\'s winter month!');
+        break;
+
+    case 'march':
+    case 'april':
+    case 'may':
+        console.log('It\'s spring month!');
+        break;   
+
+    case 'june':
+    case 'july':
+    case 'august':
+        console.log('It\'s summer month!');
+        break;
+
+    case 'september':
+    case 'october':
+    case 'november':
+        console.log('It\'s autumn month!');
         break;
 
     default:
+        console.log('You entered incorrect month name!');
         break;
 }
