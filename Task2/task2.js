e22f139f921f08bc8ae4790b546b9d5ced553fba
doher <alexandr.pletezhov@gmail.com>
2018-11-27 16:22:53 +0300

    added task 2

---
+++ Task2/task2.js
@@ -0,0 +1,20 @@
+var firstNumber;
+var secondNumber;
+var sum = 0;
+
+while (!isFinite(firstNumber) || !((firstNumber ^ 0) === firstNumber)) {
+    firstNumber = +prompt('Enter the first number:');
+}
+
+while (!isFinite(secondNumber) || !((secondNumber ^ 0) === secondNumber)) {
+    secondNumber = +prompt('Enter the second number:');
+}
+
+if (firstNumber <= secondNumber) {
+    for (var i = (secondNumber - firstNumber); i <= secondNumber; i++) {
+        sum += i;
+    }
+    console.log('Sum of numbers = ' + sum);
+} else {
+    console.log('The second number less than the first!');
+}
